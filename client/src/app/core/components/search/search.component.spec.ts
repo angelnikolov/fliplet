05933cae46536da4eca37863f75a5683e2810f44
Angel Nikolov <angel.nikolov@mentormate.com>
2018-11-22 18:27:41 +0200

    Added a test for the SearchComponent

--- client/src/app/core/components/search/search.component.spec.ts
+++ client/src/app/core/components/search/search.component.spec.ts
@@ -1,25 +1,44 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { SearchComponent } from './search.component';
 
-describe('SearchComponent', () => {
+fdescribe('SearchComponent', () => {
   let component: SearchComponent;
   let fixture: ComponentFixture<SearchComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SearchComponent ]
-    })
-    .compileComponents();
+      declarations: [SearchComponent],
+      imports: [ReactiveFormsModule],
+      schemas: [NO_ERRORS_SCHEMA]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SearchComponent);
     component = fixture.componentInstance;
+
+    spyOn(component.onSearch, 'emit');
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('#submit', () => {
+    it('if form is valid, it should call onSearch.emit', () => {
+      const validUrl = 'http://www.valid.com';
+      component.searchForm.patchValue({ searchTerm: validUrl });
+      expect(component).toBeTruthy();
+      component.submit();
+
+      expect(component.onSearch.emit).toHaveBeenCalledWith(validUrl);
+    });
+    it('if form is not valid, it should not call onSearch.emit', () => {
+      const validUrl = 'd  invalid.com';
+      component.searchForm.patchValue({ searchTerm: validUrl });
+      expect(component).toBeTruthy();
+      component.submit();
+
+      expect(component.onSearch.emit).not.toHaveBeenCalled();
+    });
   });
 });
